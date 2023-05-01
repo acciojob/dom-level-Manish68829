c18262d82ba9ead86d7d719bcdb89d00e1797a4c
Manish <121147194+Manish68829@users.noreply.github.com>
2023-05-01 23:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-//your JS code here. If required.
-let ans=document.querySelector("#level");
-alert("The level of the element is: "+ ans.innerText);
+let c= document.querySelector(".inpu");
+      console.log(c.innerText);
+    let d= document.querySelector(".input");
+   function getFormvalue() {
+    //Write your code here
+
+	alert( c.value+" "+ d.value);
+
+}
